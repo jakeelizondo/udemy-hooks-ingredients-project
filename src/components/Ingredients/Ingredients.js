31e54cd800d14f72bbfc8e0febd619b1cda55409
Jake Elizondo <jake.elizondo23@gmail.com>
2021-03-03 17:06:13 -0500

    added useState, useEffect, useRef, useReducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,109 @@
-import React from 'react';
-
+import React, { useState, useReducer, useCallback } from 'react';
+import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter((ing) => ing.id !== action.id);
+    default:
+      throw new Error('Should not get to error');
+  }
+};
+
+const httpReducer = (httpState, action) => {
+  switch (action.type) {
+    case 'SEND':
+      return { loading: true, error: null };
+    case 'RESPONSE':
+      return { ...httpState, loading: false };
+    case 'ERROR':
+      return { loading: false, error: action.error };
+    default:
+      throw new Error('Should not get to error');
+  }
+};
+
 function Ingredients() {
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  const [] = useReducer(httpReducer, { loading: false, error: null });
+  // const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
+    // setUserIngredients(filteredIngredients);
+    dispatch({ type: 'SET', ingredients: filteredIngredients });
+  }, []);
+
+  const addIngredientHandler = (ingredient) => {
+    setIsLoading(true);
+    fetch(
+      'https://udemy-ingredient-list-default-rtdb.firebaseio.com/ingredients.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(ingredient),
+        headers: { 'Content-Type': 'application/json' },
+      }
+    )
+      .then((response) => {
+        setIsLoading(false);
+        return response.json();
+      })
+      .then((data) => {
+        // setUserIngredients((prevIngredients) => [
+        //   ...prevIngredients,
+        //   { id: data.name, ...ingredient },
+        // ]);
+        dispatch({ type: 'ADD', ingredient: { id: data.name, ...ingredient } });
+      });
+  };
+
+  const removeIngredientHandler = (id) => {
+    setIsLoading(true);
+    fetch(
+      `https://udemy-ingredient-list-default-rtdb.firebaseio.com/ingredients/${id}.json`,
+      {
+        method: 'DELETE',
+      }
+    )
+      .then((response) => {
+        setIsLoading(false);
+        // setUserIngredients((prevIngredients) =>
+        //   prevIngredients.filter((ing) => ing.id !== id)
+        // );
+        dispatch({ type: 'DELETE', id });
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
+  };
+
+  const clearError = () => {
+    setError(null);
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      {error ? <ErrorModal onClose={clearError}>{error}</ErrorModal> : null}
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
 
       <section>
-        <Search />
-        {/* Need to add list here! */}
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
-const Search = React.memo(props => {
+const Search = React.memo((props) => {
+  const { onLoadIngredients } = props;
+  const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ''
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch(
+          'https://udemy-ingredient-list-default-rtdb.firebaseio.com/ingredients.json' +
+            query
+        )
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            const loadedIngredients = [];
+            for (const key in data) {
+              loadedIngredients.push({
+                id: key,
+                title: data[key].title,
+                amount: data[key].amount,
+              });
+            }
+            onLoadIngredients(loadedIngredients);
+          });
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [enteredFilter, onLoadIngredients, inputRef]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            ref={inputRef}
+            value={enteredFilter}
+            onChange={(e) => setEnteredFilter(e.target.value)}
+          />
         </div>
       </Card>
     </section>
